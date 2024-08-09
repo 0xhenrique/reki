befaaf575cc1a4f1873ac0d6b597bf5dd954b12c
Henrique Marques <allyson2308h@gmail.com>
2024-08-09 05:15:17 +0100

    start reki with lein

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to reki
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject reki "0.1.0-SNAPSHOT"
+  :description "Yes. Just another CLI tool to fetch system information."
+  :url "https://github.com/henrique-marques-vsoft/reki"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :main ^:skip-aot reki.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
